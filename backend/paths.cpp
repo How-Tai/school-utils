97dbc3d63324441922ff126054bce3018cab64c9
Tai <taisamrit12@gmail.com>
2026-09-04 20:07:50 +0700

    Register account and community routes

--- backend/paths.cpp
+++ backend/paths.cpp
@@ -1,26 +1,19 @@
 #include "paths.h"
 
 #include "frontend.h"
-
+#include "account/account.h"
+#include "community/community.h"
 #include "M2/Math/quadratic-solver/quadraticSolver.h"
-
 #include "M2/Science/planet-weight/planetWeightCalculator.h"
 #include "M2/Science/sig-fig-counting/countingSigFig.h"
-
 #include "dschool/attendance/attendanceMarker.h"
 
-
 void initServer(crow::SimpleApp& app) {
-
-    registerFrontend(app);
-
-    
-    registerMathQuadraticSolver(app);
-
-
-    registerSciPlanetWeightCalculator(app);
-    registerSciSigFigCounting(app);
-
-
-    registerDschoolAttendanceMarker(app);
+	registerFrontend(app);
+	registerAccount(app);
+	registerCommunity(app);
+	registerMathQuadraticSolver(app);
+	registerSciPlanetWeightCalculator(app);
+	registerSciSigFigCounting(app);
+	registerDschoolAttendanceMarker(app);
 }
